c9e2ed4c09b7c0a369cf457510766ae230314441
Abdulrhman Goda <abdogoda0a@gmail.com>
2025-09-14 23:18:08 +0300

    feat: Add Laravel Dusk for browser testing and implement example tests

---
+++ tests/Browser/ExampleTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class ExampleTest extends DuskTestCase
+{
+    public function testBasicExample(): void
+    {
+        $user = User::factory()->create();
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit('/login')
+                ->assertSee('Sign in to your account')
+                ->type('email', $user->email)
+                ->type('password', 'password')
+                ->press('Sign In')
+                ->assertPathIs('/profile')
+                ->assertSee('Profile')
+                ->assertSee($user->name);
+        });
+    }
+}
